9741989a39773ed0eabaed2408c5d70055bf3384
squeeko <squeeko@users.noreply.github.com>
2023-02-06 19:53:08 -0800

    Delete peroxide-smartcore directory

--- peroxide-smartcore/src/main.rs
+++
@@ -1,6 +0,0 @@
-use peroxide::fuga::*;
-
-extern crate peroxide;
-fn main() {
-    let df =  DataFrame::read_csv("/Users/jallgood/RustDataScience-Study/peroxide-smartcore/data/MalwareArtifacts.csv", ',');
-}
