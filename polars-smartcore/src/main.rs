66d41f326f43a06bcbfea46865e3da186a36d916
squeeko <squeeko@users.noreply.github.com>
2023-02-06 19:49:24 -0800

    fixed linfa problems

--- polars-smartcore/src/main.rs
+++ polars-smartcore/src/main.rs
@@ -1,4 +1,16 @@
+use nalgebra::*;
+use ndarray::array;
 use polars::prelude::*;
+use std::error::Error;
+
+extern crate nalgebra as na;
+
+use smartcore::linalg::basic::arrays::Array2;
+use smartcore::linalg::basic::matrix::DenseMatrix;
+// KNNClassifier
+use smartcore::neighbors::knn_classifier::*;
+// Various distance metrics
+use smartcore::metrics::distance::*;
 
 fn main() {
     let df = CsvReader::from_path("/Users/squeeko/RustDataScience-Study/data/MalwareArtifacts.csv")
@@ -6,32 +18,37 @@ fn main() {
         .infer_schema(None)
         .has_header(true)
         .finish();
-    //    println!("{:#?}", df.as_ref().expect("no columns").get_column_names_owned());
-    // println!("{:?}", df.as_ref().expect("No shape").shape());
-    // println!("{:?}", df.as_ref().expect("no height").height()); // rows
-    // println!("{:?}", df.as_ref().expect("no width").width() - 1); // cols
-    //    println!("{:?}", df.as_ref().expect("no slices").slice(0, 5));
-    //    println!("{:?}", df.as_ref().expect("no series").select_series(["AddressOfEntryPoint"]));
-    //    println!("{:#?}", df.as_ref().expect("no slice_par").select_by_range(0..=8).expect("shape").shape());
-    // println!("{:#?}", df.as_ref().expect("no slice_par").select_by_range(0..=7));
-    let x_df = df.as_ref().expect("no slice_par").select_by_range(0..=7);
-    println!("{:#?}", x_df.as_ref().expect("No x_df head").head(Some(5)));
-    // println!("{:#?}", x_df.as_ref().expect("No types").dtypes());
 
-    let y_df = df.as_ref().expect("no slice_par").select_by_range(8..=8);
-    println!("{:#?}", y_df.as_ref().expect("No x_df head").head(Some(5)));
+    // println!("{:#?}", df);
+    // let x = df.expect("This is not workin").select_at_idx(0);
 
-    let x_ndarray = x_df
-        .as_ref()
-        .expect("Cannot convert to ndarray")
-        .to_ndarray::<Float64Type>()
+    // let binding = df.expect("This is not workin");
+    // let x = binding.select_at_idx(0);
+
+    let x_matrix = df.as_ref().expect("This is working").select_by_range(0..=7);
+    let y_vector = df.as_ref().expect("This is working").select_by_range(8..9);
+
+    let y_df = y_vector
+        .expect("this is working")
+        .to_ndarray::<Int64Type>()
         .unwrap();
-    println!("{:#?}", x_ndarray);
 
-    let y_ndarray = y_df
-        .as_ref()
-        .expect("Cannot convert to ndarray")
-        .to_ndarray::<Float64Type>()
+    let x_df = x_matrix
+        .expect("this is working")
+        .to_ndarray::<Int64Type>()
         .unwrap();
-    println!("{:#?}", y_ndarray);
+
+    let x = DMatrix::from_iterator(137444, 8, x_df);
+    println!("{}", x);
+    let y = vec![y_df];
+    //println!("{:#?}", y);
+    // let knn = KNNClassifier::fit(&x, &y, Default::default()).unwrap();
 }
+
+// let x = array![x_df];
+
+// println!("{}", x);
+
+//
+
+// ndarray::data_repr::OwnedRepr<i64>
